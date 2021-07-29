b9016442ddb993c28f64562eff198e4db4058958
Adi Sosnovich <82078442+adisos@users.noreply.github.com>
2021-07-29 10:59:48 +0300

    issue #66: add unit testing script (#72)
    * issue #66: add unit testing script
    
    * update test-push ci pipeline
    
    * test failure in unit tests on ci pipeline
    
    * update unit tests script
    
    * remove failure in unit tests
    
    * update test script
    
    * test unit tests with failure on ci pipeline
    
    * remove test failure
    
    * add some documentation

---
+++ tests/run_unittests.py
@@ -0,0 +1,24 @@
+import unittest
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'network-config-analyzer'))
+
+
+def main():
+    loader = unittest.TestLoader()
+    start_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'tests',
+                             'classes_unit_tests')
+    suite = loader.discover(start_dir)
+    runner = unittest.TextTestRunner()
+    res = runner.run(suite)
+    # https://docs.python.org/3/library/unittest.html#unittest.TestResult
+    # res.errors: A list containing 2-tuples of TestCase instances and strings holding formatted tracebacks.
+    # Each tuple represents a test which raised an unexpected exception.
+    # res.failures: A list containing 2-tuples of TestCase instances and strings holding formatted tracebacks.
+    # Each tuple represents a test where a failure was explicitly signalled using the TestCase.assert*() methods.
+    return len(res.failures) + len(res.errors)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
