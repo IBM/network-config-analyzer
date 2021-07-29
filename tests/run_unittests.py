b032594d49117a39917f1e194bb7b125389586b5
adisos <adisos@il.ibm.com>
2021-07-29 09:51:00 +0300

    update unit tests script

--- tests/run_unittests.py
+++ tests/run_unittests.py
@@ -1,9 +1,18 @@
 import unittest
 import sys
 import os
+
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'network-config-analyzer'))
-loader = unittest.TestLoader()
-start_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'tests', 'classes_unit_tests')
-suite = loader.discover(start_dir)
-runner = unittest.TextTestRunner()
-runner.run(suite)
+
+
+def main():
+    loader = unittest.TestLoader()
+    start_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'tests',
+                             'classes_unit_tests')
+    suite = loader.discover(start_dir)
+    runner = unittest.TextTestRunner()
+    return runner.run(suite)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
