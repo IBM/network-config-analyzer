062430f91fa7c2cb5b289358e88aff6c35c9f3bb
adisos <adisos@il.ibm.com>
2021-08-02 23:53:13 +0300

    update tests script

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -493,7 +493,7 @@ def main(argv=None):
         print(f'action: {action} is not supported with test type: {test_type}')
         sys.exit(1)
 
-    sepc_file = os.path.join(os.getcwd(), test_type_and_spec_dict[test_type])
+    sepc_file = os.path.join( os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),  test_type_and_spec_dict[test_type]))
     tests_runner = TestsRunner(sepc_file, test_type, action)
     tests_runner.run_tests()
 
