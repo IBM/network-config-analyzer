26fccd28b067ffcb2e17a9059aa560ca8d540ca7
shaigue <shaigue@gmail.com>
2022-08-14 15:29:09 +0300

    finished basic auditing, timing, profiling
    Signed-off-by: shaigue <shaigue@gmail.com>

--- benchmarking/timing.py
+++ benchmarking/timing.py
@@ -1,16 +1,12 @@
 """A module for timing the benchmarks"""
 # TODO: refactor and implement
 import timeit
-from itertools import product
 
-from benchmarking.benchmarking_utils import get_all_benchmark_dirs, get_all_queries, Benchmark
+from benchmarking.benchmarking_utils import iter_all_benchmarks
 
 
 def time_all_benchmarks():
-    benchmark_dirs = get_all_benchmark_dirs()
-    queries = get_all_queries()
-    for benchmark_dir, query in product(benchmark_dirs, queries):
-        benchmark = Benchmark(benchmark_dir, query)
+    for benchmark in iter_all_benchmarks():
         runtime = timeit.repeat(benchmark.run, repeat=5, number=1)
         # TODO: do something else with the result
         print(runtime)
