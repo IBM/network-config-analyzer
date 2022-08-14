6045129429220d809d1870c7ff3a23ed28cf45ce
shaigue <shaigue@gmail.com>
2022-08-14 14:56:33 +0300

    added auditing for n peers, n policies, n namespaces

--- benchmarking/auditing.py
+++ benchmarking/auditing.py
@@ -1,26 +1,41 @@
+import inspect
 import sys
 from abc import ABC, abstractmethod
 from collections import defaultdict
+from collections.abc import Callable
 from itertools import product
 from types import FrameType
 from typing import Any
 
 from CanonicalIntervalSet import CanonicalIntervalSet
+from NetworkConfig import NetworkConfig
+from SchemeRunner import SchemeRunner
 from benchmarking.benchmarking_utils import get_all_benchmark_dirs, get_all_queries, Benchmark
 
 
-class Inspector(ABC):
+class FrameFuncMatcher:
+    def __init__(self, func: Callable):
+        self._filename = inspect.getfile(func)
+        _, self._lineno = inspect.getsourcelines(func)
+
+    def frame_matches_func(self, frame: FrameType) -> bool:
+        return frame.f_code.co_filename == self._filename and \
+               frame.f_code.co_firstlineno == self._lineno
+
+
+class FuncInspector(ABC):
     # TODO: maybe consider instead of an abstract class to be a class that
     #   takes the necessary functions in the constructor
-    def __init__(self):
+    def __init__(self, func: Callable):
         self._records = defaultdict(list)
+        self._func_matcher = FrameFuncMatcher(func)
 
     def reset(self):
         self._records = defaultdict(list)
 
     def hook(self, frame: FrameType, event: str, arg: Any):
-        if self._select_event(frame, event, arg):
-            record = self._process_event(frame, event, arg)
+        if self._func_matcher.frame_matches_func(frame):
+            record = self._process_event(frame)
             for key, value in record.items():
                 self._records[key].append(value)
 
@@ -29,12 +44,7 @@ class Inspector(ABC):
 
     @staticmethod
     @abstractmethod
-    def _select_event(frame: FrameType, event: str, arg: Any) -> bool:
-        raise NotImplementedError
-
-    @staticmethod
-    @abstractmethod
-    def _process_event(frame: FrameType, event: str, arg: Any) -> dict:
+    def _process_event(frame: FrameType) -> dict:
         raise NotImplementedError
 
     @staticmethod
@@ -43,20 +53,48 @@ class Inspector(ABC):
         raise NotImplementedError
 
 
-class ContainedInInspector(Inspector):
-    @staticmethod
-    def _select_event(frame: FrameType, event: str, arg: Any) -> bool:
-        return frame.f_code.co_name == CanonicalIntervalSet.contained_in.__name__ and \
-               'self' in frame.f_locals and \
-               isinstance(frame.f_locals['self'], CanonicalIntervalSet)
+class InspectorRunner:
+    def __init__(self, inspectors: list[FuncInspector]):
+        self.inspectors = inspectors
+
+    def run_all_inspectors(self, frame: FrameType, event: str, arg: Any) -> None:
+        for inspector in self.inspectors:
+            inspector.hook(frame, event, arg)
+
+
+class IntervalContainedIn(FuncInspector):
+
+    def __init__(self):
+        super(IntervalContainedIn, self).__init__(CanonicalIntervalSet.contained_in)
 
     @staticmethod
-    def _process_event(frame: FrameType, event: str, arg: Any) -> dict:
+    def _process_event(frame: FrameType) -> dict:
         return {
             'n_intervals': (
                 len(frame.f_locals['self']),
                 len(frame.f_locals['other'])
             )
+        }
+
+    @staticmethod
+    def _process_records(records: dict[list]) -> dict:
+        return records
+
+
+class RunQueryInspector(FuncInspector):
+
+    def __init__(self):
+        super(RunQueryInspector, self).__init__(SchemeRunner.run_queries)
+
+    @staticmethod
+    def _process_event(frame: FrameType) -> dict:
+        self: SchemeRunner = frame.f_locals['self']
+        network_config: NetworkConfig = self.network_configs['network']
+        return {
+            'n_peers': len(network_config.peer_container.peer_set),
+            'n_namespaces': len(network_config.peer_container.namespaces),
+            'n_network_policies': len(network_config.policies),
+            'policy_type': str(network_config.type)
         }
 
     @staticmethod
@@ -70,10 +108,12 @@ def audit_all_benchmarks():
 
     # TODO: add more hooks
     inspectors = [
-        ContainedInInspector()
+        IntervalContainedIn(),
+        RunQueryInspector()
     ]
-    for inspector in inspectors:
-        sys.settrace(inspector.hook)
+    inspectors_runner = InspectorRunner(inspectors)
+    # TODO: comment settrace for debugging
+    sys.settrace(inspectors_runner.run_all_inspectors)
 
     for benchmark_dir, query in product(benchmark_dirs, queries):
         benchmark = Benchmark(benchmark_dir, query)
