4cc8de2251ad3ec7b2213c5ddcfcb35303fbb1ac
Shmulik Froimovich <Shmulik.Froimovich@ibm.com>
2023-03-27 16:43:37 +0300

    added some function descriptions
    Signed-off-by: Shmulik Froimovich <Shmulik.Froimovich@ibm.com>

--- nca/Utils/ExplTracker.py
+++ nca/Utils/ExplTracker.py
@@ -7,23 +7,13 @@ from nca.Utils.Utils import Singleton
 from nca.Utils.NcaLogger import NcaLogger
 from nca.CoreDS.Peer import PeerSet
 
-class ExplDescriptor:
-    def __init__(self, name, config_code, config_file):
-        self.name = name
-        self.config_code = config_code
-        self.config_file = config_file
-
-    def __get__(self, obj, type=None) -> object:
-        return self.config_code, self.config_file
-
-    def __set__(self, obj, value) -> None:
-        raise AttributeError("Cannot change values, ExplDescriptor is read only")
-
-    def get_name(self):
-        return self.name
-
 
 class ExplPolicies:
+    """
+    ExplPolicies holds the policies affecting peers in relation to other peers.
+    That is, for each peer it holds all the peers in it's egress and ingress and the policies that has effect on the connection
+    to that peers.
+    """
     def __init__(self):
         self.egress_dst = {}
         self.ingress_src = {}
@@ -31,6 +21,12 @@ class ExplPolicies:
 
     @staticmethod
     def _add_policy(peer_set, peer_list, policy_name):
+        """
+        Adds a policy to the list of affecting policies, for each peer in the peer_set
+        :param PeerSet peer_set: a set of peers to add the policy to
+        :param dict peer_list: a list of peers that holds the policies affecting them
+        :param str policy_name: the policy to add
+        """
         for peer in peer_set:
             peer_name = peer.full_name()
             if not peer_list.get(peer_name):
@@ -38,7 +34,12 @@ class ExplPolicies:
             peer_list[peer_name].add(policy_name)
 
     def add_policy(self, policy_name, egress_dst, ingress_src):
-
+        """
+        Adds a given policy to the relevant peer lists (egress list, ingress list)
+        :param str policy_name: name of the policy
+        :param dict egress_dst: the set of egress destinations peers to add the policy too
+        :param dict ingress_src: the set of ingress source peers to add the policy too
+        """
         self.all_policies.add(policy_name)
 
         if egress_dst:
@@ -53,6 +54,8 @@ class ExplTracker(metaclass=Singleton):
     The Explainability Tracker is used for tracking the elements and their configuration
     so it will be able to specify which configurations are responsible for each peer and each connection
     or lack of connection between them.
+
+    The ExplTracker is Singletone
     """
 
     def __init__(self):
@@ -64,18 +67,39 @@ class ExplTracker(metaclass=Singleton):
         self.add_item('', 'Default-Policy', 0)
 
     def activate(self):
+        """
+        Make the ExplTracker active
+        """
         self._is_active = True
 
     def is_active(self):
+        """
+        Return the active state of the ExplTracker
+        :return: bool
+        """
         return self._is_active
 
-    def get_path_from_deployment(self, content):
-        return self.ExplDescriptorContainer[content.get('metadata').get('name')].get('path')
-
     def add_item(self, path, name, ln):
-        self.ExplDescriptorContainer[name] = {'path': path, 'line': ln}
+        """
+        Adds an item describing a configuration block
+        :param str path: the path to the configuration file
+        :param str name: the name of the configuration block (doc)
+        :param int ln: the line starting the configuration block in it's file
+        """
+        if name:
+            self.ExplDescriptorContainer[name] = {'path': path, 'line': ln}
+        else:
+            NcaLogger().log_message(f'Explainability error: configuration-block name can not be empty',
+                                    level='E')
 
     def add_peer_policy(self, peer, policy_name, egress_dst, ingress_src):
+        """
+        Add a new policy to a peer
+        :param Peer peer: peer object
+        :param srt policy_name: name of the policy
+        :param egress_dst: a list of peers that the given policy affect, egress wise.
+        :param ingress_src: a list of peers that the given policy affect, ingress wise.
+        """
         peer_name = peer.full_name()
         if self.ExplDescriptorContainer.get(peer_name):
             if not self.ExplPeerToPolicyContainer.get(peer_name):
@@ -87,6 +111,11 @@ class ExplTracker(metaclass=Singleton):
 
     @staticmethod
     def extract_peers(conns):
+        """
+        Utility function to extract the peer names held in a connectivity element
+        :param ConnectivityProperties conns:
+        :return: PeerSet src_peers, PeerSet dst_peers: sets of collected peers
+        """
         src_peers = PeerSet()
         dst_peers = PeerSet()
         for cube in conns:
@@ -96,10 +125,19 @@ class ExplTracker(metaclass=Singleton):
         return src_peers, dst_peers
 
     def set_connections(self, conns):
+        """
+        Update the calculated connections into ExplTracker
+        :param ConnectivityProperties conns: the connectivity mapping calculated by the query
+        """
         self.conns = conns
-        pass
 
     def are_peers_connected(self, src, dst):
+        """
+        Check if a given pair of peers are connected
+        :param str src: name of the source peer
+        :param str dst: name of the destination peer
+        :return: bool: True for connected, False for disconnected
+        """
         if not self.conns:
             NcaLogger().log_message(f'Explainability error: Connections were not set yet, but peer query was called',
                                     level='E')
@@ -112,7 +150,12 @@ class ExplTracker(metaclass=Singleton):
         return False
 
     def add_default_policy(self, currents, peers, is_ingress):
-
+        """
+        Add the default policy to the peers which were not affected by a specific policy.
+        :param PeerSet currents: the peers to add the policy too
+        :param PeerSet peers: the adjacent peers the default policy nakes a connection with
+        :param is_ingress: is this an ingress or egress policy
+        """
         if is_ingress:
             ingress_src = peers
             egress_dst = {}
@@ -128,7 +171,13 @@ class ExplTracker(metaclass=Singleton):
                                  )
 
     def prepare_node_str(self, direction, node_name, results):
-
+        """
+        A utility function to help format a node explainability description
+        :param str direction: src/dst
+        :param str node_name: the name of the node currently described
+        :param str results: the names of the configurations affecting this node
+        :return str: string with the description
+        """
         out = []
         if direction:
             out = [f'\n({direction}){node_name}:']
@@ -142,7 +191,16 @@ class ExplTracker(metaclass=Singleton):
         return out
 
     def explain(self, nodes):
-
+        """
+        The magic function to explain the connectivity or the LACK of it between the given nodes
+        It has 2 modes:
+            single node - if a single node is given, all the configurations on that node are displayed.
+            two nodes - if 2 nodes are given, either they hava a connection between them and the configurations responsible for
+                        the connection are displayed. or, they lack a connection, in which case, all affecting configurations
+                        on those 2 nodes are displayed.
+        :param list(str) nodes: nodes to explain
+        :return: str: the explanation out string
+        """
         out = []
         if len(nodes) < 1:
             return out
