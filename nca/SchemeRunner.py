41e13524ea5c9d563be10f8e8a919d85d3d7907e
Tanya <tatyana@il.ibm.com>
2023-04-30 13:57:44 +0300

    Added TODO comment. Removed --check_run_time in workflow runs of comparison between original and optimized solutions.
    Signed-off-by: Tanya <tatyana@il.ibm.com>

--- nca/SchemeRunner.py
+++ nca/SchemeRunner.py
@@ -191,6 +191,7 @@ class SchemeRunner(GenericYamlParser):
             self.check_fields_validity(query, 'query', allowed_elements)
             query_name = query['name']
             if self.optimized_run == 'debug':
+                # TODO - update/remove the optimization below when all queries are supported in optimized implementation
                 # optimization - currently only connectivityMap query has optimized implementation and can be compared
                 if not query.get('connectivityMap'):
                     print(f'Skipping query {query_name} since it does not have optimized implementation yet')
