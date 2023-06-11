9430f70bc02ac7abb5fab52c43a3f8ef11f48759
Tanya <tatyana@il.ibm.com>
2023-06-11 16:35:17 +0300

    Ignoring 'complex function' lint error. Returning 'passed' code for skipped queries.
    Signed-off-by: Tanya <tatyana@il.ibm.com>

--- nca/nca_cli.py
+++ nca/nca_cli.py
@@ -133,7 +133,7 @@ def _make_recursive(path_list):
     return path_list
 
 
-def run_args(args):
+def run_args(args):  # noqa: C901
     """
     Given the parsed cmdline, decide what to run
     :param Namespace args: argparse-style parsed cmdline
@@ -221,7 +221,7 @@ def run_args(args):
         # TODO - update/remove the optimization below when all queries are supported in optimized implementation
         if query_name not in implemented_opt_queries:
             print(f'Not running query {query_name} since it does not have optimized implementation yet')
-            return _compute_return_value(0, 0, 1)
+            return _compute_return_value(0, 0, 0)
 
     resources_handler = ResourcesHandler()
     network_config = resources_handler.get_network_config(_make_recursive(np_list), _make_recursive(ns_list),
