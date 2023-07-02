a6ef67cef718846376d40ac723f9b92ab35a2c5e
Tanya <tatyana@il.ibm.com>
2023-07-02 10:09:08 +0300

    Implemented optimized InterferesQuery
    Signed-off-by: Tanya <tatyana@il.ibm.com>

--- nca/SchemeRunner.py
+++ nca/SchemeRunner.py
@@ -18,7 +18,7 @@ class SchemeRunner(GenericYamlParser):
     """
 
     implemented_opt_queries = {'connectivityMap', 'equivalence', 'vacuity', 'redundancy', 'strongEquivalence',
-                               'containment', 'twoWayContainment', 'permits'}
+                               'containment', 'twoWayContainment', 'permits', 'interferes', 'pairwiseInterferes'}
 
     def __init__(self, scheme_file_name, output_format=None, output_path=None, optimized_run='false'):
         GenericYamlParser.__init__(self, scheme_file_name)
